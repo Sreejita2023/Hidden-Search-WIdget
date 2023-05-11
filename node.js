b06582432924e84a53c9426b908e5a847e9c10e9
Sreejita2023 <130359704+Sreejita2023@users.noreply.github.com>
2023-05-11 11:42:10 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,7 @@
+const btn=document.querySelector('.btn')
+const input=document.querySelector('.input')
+const search=document.querySelector('.search')
+btn.addEventListener('click',function(){
+    search.classList.toggle('change')
+    input.focus()
+})
